d0250653e62e87695528783cf82f41ae1d79f6ae
rmestizor <rmestizor@sspver.gob.mx>
2026-06-25 11:12:12 -0600

    feat: agregar manejo centralizado de errores HTTP

---
+++ SIGEMAV/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIGEMAV.Controllers;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+public class ErrorController : Controller
+{
+    [Route("Error/403")]
+    public IActionResult Error403()
+    {
+        Response.StatusCode = 403;
+        return View("403");
+    }
+
+    [Route("Error/404")]
+    public IActionResult Error404()
+    {
+        Response.StatusCode = 404;
+        return View("404");
+    }
+
+    [Route("Error/500")]
+    public IActionResult Error500()
+    {
+        Response.StatusCode = 500;
+        return View("500");
+    }
+}
--- SIGEMAV/Program.cs
+++ SIGEMAV/Program.cs
@@ -1,39 +1,30 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
+
 using SIGEMAV.Models.Data;
-using SIGEMAV.Models.Entities;
 using SIGEMAV.Services.Implementations.Financieros;
 using SIGEMAV.Services.Implementations.Maquinaria;
 using SIGEMAV.Services.Implementations.SIA;
 using SIGEMAV.Services.Interfaces.Financieros;
 using SIGEMAV.Services.Interfaces.Maquinaria;
 using SIGEMAV.Services.Interfaces.SIA;
-using System.Reflection.Emit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region Base de Datos
 
-
-
-
-
-
-//////SERVICIOS
 builder.Services.AddDbContext<BdSigeMavContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("conexion")));
 
-builder.Services.AddScoped<IMaquinariaService, MaquinariaService>();
+#endregion
 
+#region Configuración de carga de archivos
 
 builder.Services.Configure<FormOptions>(options =>
 {
-    options.MultipartBodyLengthLimit = 15 * 1024 * 1024; // 15 MB
+    options.MultipartBodyLengthLimit = 15 * 1024 * 1024;
 });
 
 builder.WebHost.ConfigureKestrel(options =>
@@ -41,79 +32,88 @@ builder.WebHost.ConfigureKestrel(options =>
     options.Limits.MaxRequestBodySize = 15 * 1024 * 1024;
 });
 
+#endregion
 
-
-builder.Services.AddScoped<MaquinariaService>();
-////SERVICIOS
-
-
+#region Servicios
 
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddDbContext<BdSigeMavContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
+
+builder.Services.AddScoped<IMaquinariaService, MaquinariaService>();
+builder.Services.AddScoped<MaquinariaService>();
 
 builder.Services.AddScoped<IDispService, DispService>();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-
-
 builder.Services.AddScoped<IObjetoGastoCatalogoService, ObjetoGastoCatalogoService>();
 builder.Services.AddScoped<IClaveAdministrativaService, ClaveAdministrativaService>();
 builder.Services.AddScoped<IProyectoService, ProyectoService>();
 builder.Services.AddScoped<IProyectoAreaService, ProyectoAreaService>();
 builder.Services.AddScoped<IOdpService, OdpService>();
 builder.Services.AddScoped<IDspService, DspService>();
-builder.Services.AddHttpClient<ISiaService, SiaService>();
 builder.Services.AddScoped<IOrdenDePago, OrdenDePagoService>();
 
+builder.Services.AddHttpClient<ISiaService, SiaService>();
+
+#endregion
+
+#region MVC
+
+builder.Services.AddControllersWithViews();
+
+#endregion
+
+#region Autenticación
 
 builder.Services
-    .AddAuthentication(
-        CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.LoginPath =
-            "/Account/Account/Login";
+        options.LoginPath = "/Account/Account/Login";
 
-        options.AccessDeniedPath =
-            "/Account/Account/AccessDenied";
+        // Página personalizada 403
+        options.AccessDeniedPath = "/Error/403";
 
-        options.ExpireTimeSpan =
-            TimeSpan.FromDays(30);
-
+        options.ExpireTimeSpan = TimeSpan.FromDays(30);
         options.SlidingExpiration = true;
     });
 
-
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Middleware
+
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // Página personalizada 500
+    app.UseExceptionHandler("/Error/500");
+
     app.UseHsts();
 }
 
+// Manejo automático de 404, 403, etc.
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
+#endregion
+
+#region Rutas
 
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
+#endregion
 
 app.Run();
-
